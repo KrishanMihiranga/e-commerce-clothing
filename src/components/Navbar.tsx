03d17ff6b59cbcdc9b1fbe359455b83d7cab233a
KrishanMihiranga <xp3cto95@gmail.com>
2025-03-05 22:54:18 +0530

    feat: top nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="bg-black text-white p-4 shadow-lg">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-xl font-bold">MyApp</Link>
-                <ul className="flex space-x-6">
-                    <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
-                    <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
-                    <li><Link to="/contact" className="hover:text-gray-300">Contact</Link></li>
-                </ul>
+        <>
+            <div className="bg-black text-white uppercase py-2 font-semibold">
+                <span>Shipping world wide</span>
             </div>
-        </nav>
+            <nav className="text-white p-4 bg-blue-900">
+                <div className="container mx-auto flex justify-between items-center">
+                    <Link to="/" className="text-xl font-bold">MyApp</Link>
+                    <ul className="flex space-x-6">
+                        <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+                        <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
+                        <li><Link to="/contact" className="hover:text-gray-300">Contact</Link></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
     );
 };
 
