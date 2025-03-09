808a0c96af6c6329c5a0371b179342dc68a9017a
KrishanMihiranga <xp3cto95@gmail.com>
2025-03-09 11:59:21 +0530

    feat: navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,79 @@
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AnimatedTextBar from "./AnimatedTextBar";
 import logo from "../../../public/images/address-logo-dark.webp"
-
+import '../../styles/navBar.css'
+import { useEffect, useState } from "react";
 const Navbar = () => {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState(location.pathname);
+
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location.pathname]);
 
     return (
 
-        <nav>
+        <nav >
             <AnimatedTextBar />
-            <div>
+            <div className="flex items-center justify-between container-layout primary-text">
                 <img
                     src={logo}
                     alt="Address Clothing logo"
                     title="Address Clothing Company"
+                    className="w-[15rem]"
                 />
+                <ul className="flex gap-[1.9rem]">
+                    <li>
+                        <Link
+                            to="/"
+                            className={`hover:text-gray-500 ${activeLink === "/" ? "selected-nav" : ""}`}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/collections/mens"
+                            className={`hover:text-gray-500 ${activeLink === "/collections/mens" ? "selected-nav" : ""}`}
+                        >
+                            Men
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/collections/women"
+                            className={`hover:text-gray-500 ${activeLink === "/collections/women" ? "selected-nav" : ""}`}
+                        >
+                            Women
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/collections/accessories"
+                            className={`hover:text-gray-500 ${activeLink === "/collections/accessories" ? "selected-nav" : ""}`}
+                        >
+                            Accessories
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/collections/gift-cards"
+                            className={`hover:text-gray-500 ${activeLink === "/collections/gift-cards" ? "selected-nav" : ""}`}
+                        >
+                            Gift cards
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="flex gap-5">
+                    <div className="relative border !px-[3rem] !py-[.7rem] text-center rounded-3xl">
+                        <span className="absolute -top-[.8rem] right-0 bg-black rounded-full w-[2rem] h-[2rem] text-white text-sm flex flex-col items-center justify-center">0</span>
+                        Bag
+                    </div>
+                    <div className="border text-center !px-[3rem] !py-[.7rem] rounded-3xl bg-black text-white">Login</div>
+                </div>
             </div>
-            {/* <div className="flex justify-between items-center container">
-                    <Link to="/" className="font-bold text-xl">MyApp</Link>
-                    <ul className="flex space-x-6">
-                        <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
-                        <li><Link to="/about" className="hover:text-gray-300">About</Link></li>
-                        <li><Link to="/contact" className="hover:text-gray-300">Contact</Link></li>
-                    </ul>
-                </div> */}
         </nav>
 
     );
